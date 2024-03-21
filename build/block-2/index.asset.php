f1767dd22c996f7ce7b3a1da490918ef098795e4
roeljohnbobis <roeljohn@users.noreply.github.com>
2024-03-21 19:24:43 +0800

    new rj-block-category was created to seperate my blocks to other blocks

--- build/block-2/index.asset.php
+++ build/block-2/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5d2099cc870ebca996da');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd1daa33bf864e360b915');
